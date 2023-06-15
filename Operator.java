a6fdc6996f15241698d98553ab60a5e8ff13deee
vrutik <108508556+vrutik8@users.noreply.github.com>
2023-06-15 18:49:30 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Operator{
+    public static void main(String args[]){
+        int a = 10;
+        int b = 5;
+        System.out.println("addition is "+ (a+b));
+
+    }
+}
